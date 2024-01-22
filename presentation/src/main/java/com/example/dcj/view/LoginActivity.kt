029326b7871adad3d22a96f034364996b44a3096
KKdohyeong <dohyeong254@naver.com>
2024-01-22 16:20:45 +0900

    로그인 activity 페이지 만듦

--- presentation/src/main/java/com/example/dcj/view/LoginActivity.kt
+++ presentation/src/main/java/com/example/dcj/view/LoginActivity.kt
@@ -3,23 +3,29 @@ package com.example.dcj.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.dcj.R
+import com.example.dcj.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.Firebase
+
+
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var binding : ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-// Initialize Firebase Auth
-        auth = Firebase.auth
-
         setContentView(R.layout.activity_login)
+
+
+
+
     }
 
     override fun onStart() {
         super.onStart()
+
     }
 }
