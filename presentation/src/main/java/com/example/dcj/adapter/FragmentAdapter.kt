51c23673e6ee251f147b74a7af33aad60e9a025a
Sonchaehoon <corb7@naver.com>
2024-01-22 16:17:29 +0900

    파일명 수정

--- presentation/src/main/java/com/example/dcj/adapter/FragmentAdapter.kt
+++ presentation/src/main/java/com/example/dcj/adapter/FragmentAdapter.kt
@@ -2,14 +2,14 @@ package com.example.dcj.adapter
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.dcj.view.FragmentA
-import com.example.dcj.view.FragmentB
+import com.example.dcj.view.InProgFragment
+import com.example.dcj.view.BeforeStartFragment
 
 class FragmentAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
 
     private val fragmentList = listOf(
-        FragmentA(),
-        FragmentB()
+        InProgFragment(),
+        BeforeStartFragment()
     )
 
     override fun getItemCount(): Int = fragmentList.size
