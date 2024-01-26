6a64213dff2172fafbc1e3016c7f304b94e3830f
Leo <zxcvbas137@gmail.com>
2024-01-26 20:27:48 +0900

    찐일단

--- presentation/src/main/java/com/example/dcj/view/CertificationFragment.kt
+++ presentation/src/main/java/com/example/dcj/view/CertificationFragment.kt
@@ -2,11 +2,13 @@ package com.example.dcj.view
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
 import com.example.dcj.R
@@ -39,6 +41,12 @@ class CertificationFragment : Fragment() {
                 else -> null
             }
         }.attach()
+
+        view.findViewById<Button>(R.id.curBtn).setOnClickListener {
+            val intent = Intent(activity, DetailActivity::class.java)
+            startActivity(intent)
+
+        }
 
         return view
     }
