7f00c05d732eec0a58ebb983c6a1d27c7e8fafcb
Sonchaehoon <corb7@naver.com>
2024-01-29 16:16:11 +0900

    Detail_Activity로 통합

--- presentation/src/main/java/com/example/dcj/view/DetailActivity.kt
+++ presentation/src/main/java/com/example/dcj/view/DetailActivity.kt
@@ -2,17 +2,36 @@ package com.example.dcj.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.example.dcj.R
+import com.example.dcj.databinding.ActivityDetailBinding
 import com.google.firebase.firestore.FirebaseFirestore
 
 class DetailActivity : AppCompatActivity() {
+    private var challengename: String? = null
+    private var challengedetail: String? = null
+    private var _binding: ActivityDetailBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        _binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 인텐트에서 데이터 가져오기
+        challengename = intent.getStringExtra("challengeName")
+        challengedetail = intent.getStringExtra("challengeDetail")
+
+        with(binding){
+            textView3.text = "${challengename}"
+            textView6.text="${challengedetail}"
+        }
     }
 
-    fun favoriteEvent(position : Int) {
-
-
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
